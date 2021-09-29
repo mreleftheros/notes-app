4141b8f10f209c5f0b59d6d1c9a97a9dc173d280
eleftheros <eleftheros_all@protonmail.com>
2021-09-29 16:35:56 +0300

    Create saveNote in storage.js and update handleNotesContainerClick method in ui

--- src/js/storage.js
+++ src/js/storage.js
@@ -2,4 +2,12 @@ const getNotes = () => {
   return JSON.parse(localStorage.getItem("notes")) || [];
 };
 
-export { getNotes };
+const saveNote = text => {
+  const notes = getNotes();
+
+  notes.push(text);
+
+  localStorage.setItem("notes", JSON.stringify(notes));
+}
+
+export { getNotes, saveNote };
--- src/js/ui.js
+++ src/js/ui.js
@@ -1,14 +1,14 @@
 import note from "./note";
+import { saveNote } from "./storage";
 
 class Ui {
   constructor() {
     this.newBtn = document.getElementById("newBtn");
-    this.viewBtn = document.getElementById("viewBtn");
     this.lightBtn = document.getElementById("lightBtn");
     this.notesContainer = document.getElementById("notesContainer");
     this.icons = {
       edit: "&#x1F4DD;",
-      save: ["&#x2714;", "&#x2705;"],
+      save: "&#x2714;",
       delete: "&#x1F5D1;"
     }
   }
@@ -20,22 +20,29 @@ class Ui {
     const divElement = document.createElement("div");
     divElement.classList.add("main__notes-container__note");
     divElement.setAttribute("data-note", note.notesIndex);
+    let index = note.notesIndex + 1;
     note.notesIndex++;
+    let icon;
 
     if (e === undefined) {
       divElement.classList.add("locked");
+      icon = this.icons.edit;
+    }
+    else {
+      icon = this.icons.save;
     }
 
     text = text || "";
 
     let html = `
       <div class="main__notes-container__note__header">
+        <h2 class="main__notes-container__note__header__title">Note ${index}</h2>
         <div class="main__notes-container__note__header__tools">
           <button type="button" class="main__notes-container__note__header__tools__btn" id="editBtn">
-            <span class="main__notes-container__note__header__tools__btn__icon">${this.icons.edit}</span>
+            <span class="main__notes-container__note__header__tools__btn__icon edit">${icon}</span>
           </button>
           <button type="button" class="main__notes-container__note__header__tools__btn" id="deleteBtn">
-            <span class="main__notes-container__note__header__tools__btn__icon">${this.icons.delete}</span>
+            <span class="main__notes-container__note__header__tools__btn__icon delete">${this.icons.delete}</span>
           </button>
         </div>
       </div>
@@ -49,6 +56,23 @@ class Ui {
     divElement.innerHTML = html;
 
     this.notesContainer.appendChild(divElement);
+  }
+  handleNotesContainerClick(e) {
+    // click edit
+    if (e.target.tagName === "SPAN" && e.target.classList.contains("edit")) {
+      let note = e.target.parentElement.parentElement.parentElement.parentElement;
+
+      if (note.classList.contains("locked")) {
+        note.classList.remove("locked");
+        e.target.innerHTML = this.icons.save;
+      }
+      else {
+        let text = note.lastElementChild.firstElementChild.value;
+        note.classList.add("locked");
+        e.target.innerHTML = this.icons.edit;
+        return saveNote(text);
+      }
+    }
   }
 }
 
