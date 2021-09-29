ecda17d697124518bdd4b250a065dd87c06b8c99
eleftheros <eleftheros_all@protonmail.com>
2021-09-29 12:02:41 +0300

    Update new-btn ui

---
+++ src/js/index.js
@@ -0,0 +1,14 @@
+// initializes the app
+const init = () => {
+  const newBtn = document.getElementById("newBtn");
+
+  // creates new note
+  const createNote = () => {
+    
+  };
+
+  // event listeners
+  newBtn.addEventListener("click", createNote);
+};
+
+document.addEventListener("DOMContentLoaded", init);
